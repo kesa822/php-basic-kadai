a5a4adc276f174e9ccb0b9cf5c17b7f7074eb42f
kesa822 <k.sano0616@gmail.com>
2025-04-21 23:39:58 +0900

    Add files via upload

---
+++ kadai_007/associative-array.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="ja">
+
+  <head>
+    <meta charset="UTF-8">
+    <title>連想配列を作ってキーと値を出力しよう</title>
+  </head>
+
+  <body>
+    <p>
+      <?php
+        $vegetable_data = ['name' => 'onion', 'price' => 200, 'weight' => '160'];
+
+        print_r($vegetable_data);
+
+        echo '<br>';
+
+        
+
+      ?>
+    </p>
+  </body>
+</html>
